8dcaa87e6b83c7aeb89aecd0b49d3943c7bcaa89
unknown <sunthodeb787@gmail.com>
2025-08-05 23:02:21 +0600

    Website firebase config update

--- src/AllComponent/AuthoncationAll/SingUp/SingUp.jsx
+++ src/AllComponent/AuthoncationAll/SingUp/SingUp.jsx
@@ -93,7 +93,7 @@ const SingUp = () => {
                 // user Update 
                 updateProfile(createUser, { displayName: FirstName })
                     .then(() => {
-                        let saveUser = { name: createUser.displayName, LastName: LastName, BusinessName, Address, Phone, Password, email: createUser.email, photo: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&q=80&w=1480&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", userId: Math.round(Math.random() * 99999999).toString(), role: "admin", status: "pending", Districts, PoliceStations, date }
+                        let saveUser = { name: createUser.displayName, LastName: LastName, BusinessName, Address, Phone, Password, email: createUser.email, photo: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&q=80&w=1480&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", userId: Math.round(Math.random() * 99999999).toString(), role: "user", status: "pending", Districts, PoliceStations, date }
 
                         // console.log(saveUser)
 
